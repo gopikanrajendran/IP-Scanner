f8a5b630a471769cffc5fa90e71ba3a84e9b7293
gopikanrajendran <135725172+gopikanrajendran@users.noreply.github.com>
2024-02-23 18:05:29 +0530

    Add files via upload

---
+++ ip-scanner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "You forgot an IP address!"
+	echo "Syntax: ./ip-scanner.sh 192.168.1"
+	echo "cat ip"
+	
+
+else
+	chmod +x sweep.sh
+	./sweep.sh $1 > /tmp/qwerty.txt
+	sleep 10s
+	cat /tmp/qwerty.txt | uniq > ip.txt
+	cat -n ip.txt
+	rm /tmp/qwerty.txt
+fi
